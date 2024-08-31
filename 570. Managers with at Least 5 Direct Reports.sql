27b6bb5d8089e6a998470fc44cbbe0aa7c5c18aa
Денис <127181531+DenMeow@users.noreply.github.com>
2024-08-31 13:54:00 +0300

    Create 570. Managers with at Least 5 Direct Reports.sql

---
+++ 570. Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,7 @@
+SELECT e1.name FROM Employee e1
+JOIN (
+    SELECT managerId, COUNT(*) AS directReports
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) e2 ON e1.id = e2.managerId
